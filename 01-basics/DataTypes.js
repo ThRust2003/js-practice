63dc650a9023c5323bf74facdb333d5057dbbab3
ThRust2003 <rohanyadav10rbl@gmail.com>
2025-03-22 16:52:09 +0000

    Datatypes

---
+++ 01-basics/DataTypes.js
@@ -0,0 +1,11 @@
+
+let score="77"
+let scoreB="45qwerty"
+let scoreC=undefined
+console.log(typeof (Number(score)))
+console.log(typeof ((scoreB)))//NaN 
+console.log(typeof ((scoreC)))
+console.log(4+7+"8")
+console.log(typeof (Number(score)))
+console.log(typeof (Number(scoreB)))//NaN 
+console.log(typeof ((scoreC)))
