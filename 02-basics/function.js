db028ad025054e94e6a3a38650d2f875d9187cbb
ThRust2003 <rohanyadav10rbl@gmail.com>
2025-03-30 05:53:52 +0000

    fun1

---
+++ 02-basics/function.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(number1,number2=2){
+    console.log(number1+number2);
+}
+const result =addTwoNumbers(3,);
+console.log("Result:",result);
+function calculateCartPrice(A,B,...num1){//spread operator 
+return num1
+}
+console.log(calculateCartPrice(200,400,530,900))
