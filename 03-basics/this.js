46a30e8014698b3a305d9a9f52eb98f98a68dae5
ThRust2003 <rohanyadav10rbl@gmail.com>
2025-03-30 19:02:30 +0000

    this

---
+++ 03-basics/this.js
@@ -0,0 +1,32 @@
+  const user={
+    username:"rohan",
+    price:999,
+    welcomemessage:function(){
+        console.log(`${this.username}` )
+        console.log(this)
+    }
+
+  }
+  user.welcomemessage()
+  user.username="r"
+  user.welcomemessage()
+  user.username="rohan Yadav"
+  user.welcomemessage()
+//   this inside an object method always refers to the current state of the object at the time the method is called.
+//   Local Scope (Inside the function)
+
+//   It first checks if username is defined inside welcomemessage.
+  
+//   In this case, username is not defined inside the function, so JavaScript moves to the next scope.
+  
+//   Object Scope (Inside the object)
+  
+//   JavaScript does not automatically check object properties (user.username).
+  
+//   Instead, you need to explicitly use this.username to reference the object property.
+  
+//   Global Scope
+  
+//   If JavaScript doesn't find username inside welcomemessage, it checks the global scope.
+  
+//   Since username is not declared globally, it throws:
