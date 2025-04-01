c41a5bb30c66ee01b0d2505230188ae4b48a7b42
ThRust2003 <rohanyadav10rbl@gmail.com>
2025-04-01 17:58:36 +0000

    changes

---
+++ 03-basics/truthy.js
@@ -0,0 +1,8 @@
+// falsy values" 0,-0,"",BigInt 0n,null,undefined,NaN"
+// truthy value "0",'false'," ",[],{},function(){}
+const empty={}
+if(!Object.keys(empty).length){
+    console.log("dbdn")
+}
+let g;
+g=null??3 
---
+++ 04-basics/loops.js
@@ -0,0 +1,8 @@
+let a=0;
+const b=4;
+for( a=0;a<9;a+=2){
+    console.log(a)
+}
+do{
+    console.log(a++);
+}while(a<100)
